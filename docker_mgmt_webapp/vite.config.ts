a2a26f84ae7c84f5afe67eea640c5b718253aa92
neokkk <seven3126@gmail.com>
2023-07-29 15:46:24 +0000

    feat: 패키지 추가 설정

--- docker_mgmt_webapp/vite.config.ts
+++ docker_mgmt_webapp/vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   server: {
     port: 3000,
   },
